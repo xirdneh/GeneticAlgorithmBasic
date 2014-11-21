06d88bbddd022abef14ada418c9a9227d995d376
Josue Balandrano Coronel <xirdneh@gmail.com>
2014-11-20 23:06:50 -0600

    Update

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+#! /usr/bin/env python2.7
 from random import choice, randint, sample, shuffle, random
 from time import asctime as time
 from sys import argv
 
+'''
+    initialize an individual for the population of size @size
+'''
 def random_individual(size):
     return [randint(0, 1) for i in range(size)]
 
+'''
+    calculates the fitness of an individual by adding all the numbers. If it's all 1s the it will be 64.
+'''
 def fitness(individual):
     return sum(individual)
 
+
+'''
+    @population: population to grab the parents from
+    It will grab 10 random parents from a given population
+'''
 def select_parents(population):
     parents = []
     for i in range(10):
@@ -17,6 +29,10 @@ def select_parents(population):
         parents.append(fit_parents[0][1])
     return parents
 
+'''
+    @parents: array with parent individuals.
+    It will create pairs of parents from a given array of parents. Randomly, of course.
+'''
 def get_parent_pairs(parents):
     pairs = []
     shuffle(parents)
@@ -24,6 +40,10 @@ def get_parent_pairs(parents):
         pairs.append([parents[i], parents[i + 1]])
     return pairs
 
+'''
+    @pair: Pair of individuals which are the parents.
+    Create two random numbers and are used as two points for the corssover
+'''
 def crossover(pair):
     i = randint(0, len(pair[0]) - 1)
     j = randint(0, len(pair[0]) - 1)
@@ -32,6 +52,11 @@ def crossover(pair):
     else:
         return [pair[0][:j] + pair[1][j:i] + pair[0][i:], pair[1][:j] + pair[0][j:i] + pair[1][i:]]
 
+'''
+    @pairs: list with pairs of parents
+    @cp: crossover probability
+    Will create an offspring if the random number is less than the cp
+'''
 def get_offsprings(pairs, cp):
     offsprings = []
     while len(offsprings) < POPULATION_SIZE:
@@ -43,6 +68,11 @@ def get_offsprings(pairs, cp):
                 offsprings.append(p)
     return offsprings
 
+'''
+    @individual: Individual to mutate
+    @mr: mutation ratio
+    It will traverse each bit and flip it if the random number is less than the rm
+'''
 def mutate(individual, mr):
     i = 0
     for b in individual:
@@ -55,6 +85,11 @@ def mutate(individual, mr):
         i += 1
     return individual
 
+'''
+    @offsprings: list of offsprings
+    @mr: mutation ratio
+    mutate all of the offsprings in a given list
+'''
 def get_mutation(offsprings, mr):
     mutated = []
     for o in offsprings:
@@ -99,11 +134,15 @@ if __name__ == '__main__':
             f.write("\nGeneration number where we reached success: {0} \n Iteration {1}\n".format(i, j))
             f.close()
             result.append([i, j])
-    maximum = max(result, key=lambda r:result[0])
-    minimum = min(result, key=lambda r:result[0])
+        else:
+            result.append([i, j])
+    print result
+    maximum = max(result, key=lambda r:r[0])
+    minimum = min(result, key=lambda r:r[0])
     average = 0
     for r in result:
-        average += result[0]
+        average += r[0]
     average = average / len(result)
     f = open("result.txt", "a")
-    f.write("\nMaximum generation reached: {0} \n Minimum generation reached: {1} \n Average generation reached {2}\n".format(maximum, minimum, average)
+    f.write("\nMaximum generation reached: {0} \n Minimum generation reached: {1} \n Average generation reached {2}\n".format(maximum, minimum, average))
+    f.close()
